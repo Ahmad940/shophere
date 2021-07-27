47444a2d6042966f92bce6f4179a941a915a111d
abdrhmnsaeed <52531081+abdrhmnsaeed@users.noreply.github.com>
2021-07-27 04:40:52 -0700

    added cart

---
+++ pages/cart/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    <p>Cart Page</p>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  name: "Cart",
+  head: {
+    title: "Cart"
+  }
+});
+</script>
+
+<style scoped></style>
